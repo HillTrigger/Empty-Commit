6075b621f5fbf4af5694500c51bd1b2a328a3d93
HillTrigger <HillTrigger@proton.me>
2025-03-23 16:59:28 +0600

    поменял импорты и поставил scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+	css: ['~/assets/styles/main.scss'],
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/eslint',
     '@formkit/auto-animate',
     '@pinia/nuxt'
   ]
-})
+});
