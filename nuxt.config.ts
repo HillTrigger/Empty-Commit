505007c02c0ffcb7c60db35d038d97730eac6b8a
HillTrigger <HillTrigger@proton.me>
2025-03-24 13:16:10 +0600

    проблема с CRP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+    head: {
+      meta: [
+        {
+          'http-equiv': 'Content-Security-Policy',
+          content: `default-src 'self'; 
+                    script-src 'self' http://localhost:3000 'unsafe-eval' 'unsafe-inline'; 
+                    style-src 'self' 'unsafe-inline'; 
+                    img-src 'self' data:; 
+                    font-src 'self'  http://localhost:3000; 
+                    connect-src 'self'; 
+                    object-src 'none'; 
+                    frame-src 'none';`
+        }
+      ]
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 	css: ['~/assets/styles/main.scss'],
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
+<script setup>
+// import axios from 'axios';
+
+// const { data: posts } = await useAsyncData('posts', () =>
+//   axios.get('https://directus.api.hilltrigger.ru/items/posts')
+//     .then(response => response.data)
+// );
+
+</script>
+
 <template>
   <div>
-    <h1 class="text-text-primary bg-background">Hello world!</h1>
+    <h1>Список постов</h1>
+    
+    <!-- Отображаем посты -->
+    <!-- <ul>
+      <li v-for="post in posts" :key="post._path">
+        <NuxtLink :to="post._path">
+          <h2>{{ post.title }}</h2>
+          <p>{{ post.description }}</p>
+        </NuxtLink>
+      </li>
+    </ul> -->
   </div>
 </template>
