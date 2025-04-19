2b3ed8c308653d08673f76d94948d187d13b5183
HillTrigger <HillTrigger@proton.me>
2025-04-20 01:43:09 +0600

    Добавил иконку для rss

--- components/app/footer/index.vue
+++ components/app/footer/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="mt-auto border-t border-borderColor p-4 flex align-center">
+  <footer class="mt-auto border-t border-borderColor p-4 flex items-center gap-4">
     <p>Проект создан с целью изучения Vue и Nuxt. Автор: <NuxtLink
       class="font-medium text-textAccent"
       to="https://github.com/HillTrigger" 
@@ -8,5 +8,8 @@
       external
     >HillTrigger</NuxtLink>
     </p>
+    <NuxtLink target="_blank" to="rss" class="hover:text-orange-500">
+      <SvgRss class="w-4 h-4 min-w-4 min-h-4"/>
+    </NuxtLink>
   </footer>
 </template>
